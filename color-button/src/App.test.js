36a0bb82cc24151ed775706c532e0c2a3c19c757
humbertoromanojr <astronomi@gmail.com>
2022-02-22 20:41:03 -0300

    button has correct initial color

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react-native/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and text of  'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+
+/* test('button has correct initial text', () => {
+  
+}); */
+
+test('button turns blue when clicked', () => {
+  
 });

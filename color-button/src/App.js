37e455dad769fc19be1ca5b95e942dc399050d5e
humbertoromanojr <astronomi@gmail.com>
2022-02-22 21:15:36 -0300

    Test and code button click

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ buttonColor, setButtonColor ] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
     <div>
-      <button style={{ backgroundColor: 'red' }}>Change to blue</button>
+      <button 
+      onClick={() => setButtonColor(newButtonColor)} 
+      style={{ backgroundColor: buttonColor }}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
